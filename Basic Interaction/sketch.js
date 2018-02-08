ba162259b37a7a6112ad0ee0411cbe938cb8ddcf
BillyThePirate69 <36053848+BillyThePirate69@users.noreply.github.com>
2018-02-08 14:34:10 -0600

    Update sketch.js

--- Basic Interaction/sketch.js
+++ Basic Interaction/sketch.js
@@ -46,8 +46,8 @@ function mouseClicked(){
 
 function deviceShaken(){
   var s = 'NUUUUUUUT'
-  textFront("cursive");
+  textFront("Comic Sans MS");
   fill(random(255), random(255), random(255);
   textSize(50);
-  text(s, random(2000), random(2000), random(2000), random(2000));
+  text(s, random(2000), random(2000));
 }
