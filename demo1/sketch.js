a06aa5210deebb6e7f5213f29913ff3063d1c21a
BillyThePirate69 <36053848+BillyThePirate69@users.noreply.github.com>
2018-02-12 15:07:23 -0600

    Add files via upload

---
+++ demo1/sketch.js
@@ -0,0 +1,17 @@
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  background(255);
+}
+
+
+function draw(){
+  if (mouseX < windowWidth/2){
+    fill (random(0, 255));
+    rect (random(0, width), random(0, height), random(50,200), random(50,200));
+  }
+  if (mouseX > windowWidth/2){
+    fill (random(255), random(255), random(255));
+    ellipse (random(0, width), random(0, height), random(50,200));
+  }
+}
