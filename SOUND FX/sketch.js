6cf8eca47e7480cb012f3c0efaaf234bed5c2b28
BillyThePirate69 <36053848+BillyThePirate69@users.noreply.github.com>
2018-03-15 14:57:44 -0600

    Add files via upload

---
+++ SOUND FX/sketch.js
@@ -0,0 +1,21 @@
+let tyler1;
+let xd;
+
+function preload(){
+  tyler1 = loadSound("assets/Tyler1 Scream.mp3");
+  xd = loadSound("assets/REEEEEEEEEE.mp3");
+  ree = loadImage("images/ree.jpg");
+}
+
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  tyler1.setVolume(1.0);
+
+
+}
+
+function mouseClicked(){
+  xd.play();
+  image(ree,0,0,windowWidth,windowHeight);
+}
