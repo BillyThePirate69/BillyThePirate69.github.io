80b0f0065375e15192cb5348ff565a57eb6569ed
BillyThePirate69 <36053848+BillyThePirate69@users.noreply.github.com>
2018-05-28 15:19:27 -0600

    Add files via upload

--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -1,24 +1,39 @@
-let sprites;
-let game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+let gameScene = new Phaser.Scene('Game');
+
+var config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+let game = new Phaser.Game(config);
 
 function preload(){
-  game.load.spriteSheet('test', 'Test/xd.png', 100, 100);
+  this.load.atlas('background', 'images/rpg.png', 'images/rpg.json');
+  //left
+  this.load.spritesheet('Left', 'sprite/walkleft/0.png', { frameWidth: 34, frameHeight: 34 }
+  );
+
 }
+
 function create(){
-  sprites = this.game.add.sprite(10, 30, 'test');
-  sprites.frame = 3;
+  let background = this.add.sprite(50, 50, 'background', 'rpg.png');
+  this.add.sprite(130, 130, 'Left');
+  this.anims.create({
+      key: 'moveLeft',
+      frames: this.anims.generateFrameNumbers('Left', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+  });
 }
+
 function update(){
-  if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-    sprite.x -=4;
-  }
-  if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-    sprite.x +=4;
-  }
-  if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-    sprite.y -=4;
-  }
-  if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-    sprite.y +=4;
-  }
+//  if (this.cursors.left.isDown){
+  //  player.setVelocityX(-160);
+//    player.anims.play('left', true)
+//  }
 }
