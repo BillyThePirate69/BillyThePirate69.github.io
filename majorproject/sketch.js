2ca5bf418772bcbe78eb5313cb4ae238ff83285f
BillyThePirate69 <36053848+BillyThePirate69@users.noreply.github.com>
2018-05-30 15:22:04 -0600

    Add files via upload

--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -5,15 +5,20 @@ let layer;
 
 
 function preload() {
-  game.load.spritesheet('sprite', 'sprite/walkleft/0.png', 32, 34);
-  game.load.image('tileset', 'map/tileset.png');
-  game.load.tilemap('map', 'map/rpg.json', null, Phaser.Tilemap.TILED_JSON);
+  game.load.spritesheet('sprite', 'sprite/walkleft/0.png', 34, 34);
+  game.load.image('tileset', 'map/maptile.png');
+  game.load.tilemap('map', 'map/house.json', null, Phaser.Tilemap.TILED_JSON);
 
 }
 
 function create() {
   map = this.game.add.tilemap('map');
-  map.addTilesetImage('tileset', 'tileset');
+  layer = map.createLayer('grass');
+  map.createLayer('house')
+  map.addTilesetImage('maptile', 'tileset');
+  // sprite
+  player = this.game.add.sprite(300, 400, 'sprite');
+  player.scale.setTo(2, 2);
 
 }
 
