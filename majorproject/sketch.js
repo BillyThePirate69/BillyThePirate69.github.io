281b82c9b6d8be366846a18d8bea7ea23f9eb1f5
BillyThePirate69 <36053848+BillyThePirate69@users.noreply.github.com>
2018-06-06 15:20:05 -0600

    Add files via upload

--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -2,13 +2,35 @@ let game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 let player;
 let map;
 let layer;
-let controls = {};
 let x;
 let y;
+let playerState = {
+  player: {
+    xDest: null,
+    yDest: null
+  },
+  npc: {
 
-
-
-
+  }
+}
+function movePlayer(){
+  if (Math.floor(this.x / 10) == Math.floor(this.xDest / 10)) {
+    this.body.velocity.x = 0;
+  } else if (Math.floor(this.x) < Math.floor(this.xDest)) {
+    this.body.velocity.x = this.speed;
+    this.scale.x = -1;
+  } else if (Math.floor(this.x) > Math.floor(this.xDest)) {
+    this.body.velocity.x = -this.speed;
+    this.scale.x = 1;
+  }
+  if (Math.floor(this.y / 10) === Math.floor(this.yDest / 10)) {
+    this.body.velocity.y = 0;
+  } else if (Math.floor(this.y) < Math.floor(this.yDest)) {
+    this.body.velocity.y = this.speed;
+  } else if (Math.floor(this.y) > Math.floor(this.yDest)) {
+    this.body.velocity.y = -this.speed;
+  }
+}
 
 function preload() {
   game.load.spritesheet('sprite', 'sprite/walkleft/0.png', 34, 34);
@@ -18,33 +40,50 @@ function preload() {
 }
 
 function create() {
+  //map
   map = this.game.add.tilemap('map');
-  layer = map.createLayer('grass');
-  map.createLayer('house')
+  game.world.setBounds(0,0,500,500);
+  this.layer = map.createLayer('grass');
+  this.house = map.createLayer('house');
   map.addTilesetImage('maptile', 'tileset');
+
   // sprite
-  player = this.game.add.sprite(486, 384, 'sprite');
-  player.scale.setTo(2, 2);
-  player.frame = 0;
-  player.animations.add('left', [0], 2, true);
-  player.animations.add('left', [1, 2, 3], 2, false);
+  this.player = this.game.add.sprite(game.world.centerX, game.world.centerY                                         , 'sprite');
+  this.player.scale.setTo(2, 2);
+  this.player.anchor.set(0.5, 0.5);
+  this.player.frame = 0;
+  this.player.animations.add('left', [0], 2, true);
+  this.player.animations.add('left', [1, 2, 3], 2, false);
+  this.physics.arcade.enable(this.player);
+  // capture
+  game.input.activePointer.capture = true;
 
 
 }
 
 function update() {
   let k = this.input.keyboard;
+  this.physics.arcade.collide(this.player, this.house);
   if(k.isDown(37)){
-    player.x -= 2;
-    player.animations.play('left');
+    this.player.x -= 2;
+    this.player.animations.play('left');
+      this.player.scale.setTo(2, 2);
   }
   else if(k.isDown(39)){
-    player.x += 2;
+    this.player.x += 2;
+    this.player.scale.setTo(-2, 2);
+    this.player.animations.play('left');
   }
   else if(k.isDown(38)){
-    player.y -= 2;
+    this.player.y -= 2;
   }
   else if(k.isDown(40)){
-    player.y += 2;
+    this.player.y += 2;
   }
+  // mouse/touchscreen input
+  if(game.input.activePointer.isDown){
+    this.player.xDest = game.input.x;
+    this.player.yDest = game.input.y;
+  }
+  //this.movePlayer(game.input.x, game.input.y);
 }
