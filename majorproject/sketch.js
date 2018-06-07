2880755c8c0bac76b993e3e46cec75d3ce10cd77
BillyThePirate69 <36053848+BillyThePirate69@users.noreply.github.com>
2018-06-07 15:21:17 -0600

    Add files via upload

--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -1,36 +1,15 @@
-let game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-let player;
+let game = new Phaser.Game(480, 360, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 let map;
 let layer;
+let self = this;
 let x;
 let y;
-let playerState = {
-  player: {
-    xDest: null,
-    yDest: null
-  },
-  npc: {
+let player = {
+  xDest: null,
+  yDest: null
+}
+let speed;
 
-  }
-}
-function movePlayer(){
-  if (Math.floor(this.x / 10) == Math.floor(this.xDest / 10)) {
-    this.body.velocity.x = 0;
-  } else if (Math.floor(this.x) < Math.floor(this.xDest)) {
-    this.body.velocity.x = this.speed;
-    this.scale.x = -1;
-  } else if (Math.floor(this.x) > Math.floor(this.xDest)) {
-    this.body.velocity.x = -this.speed;
-    this.scale.x = 1;
-  }
-  if (Math.floor(this.y / 10) === Math.floor(this.yDest / 10)) {
-    this.body.velocity.y = 0;
-  } else if (Math.floor(this.y) < Math.floor(this.yDest)) {
-    this.body.velocity.y = this.speed;
-  } else if (Math.floor(this.y) > Math.floor(this.yDest)) {
-    this.body.velocity.y = -this.speed;
-  }
-}
 
 function preload() {
   game.load.spritesheet('sprite', 'sprite/walkleft/0.png', 34, 34);
@@ -42,19 +21,22 @@ function preload() {
 function create() {
   //map
   map = this.game.add.tilemap('map');
-  game.world.setBounds(0,0,500,500);
   this.layer = map.createLayer('grass');
   this.house = map.createLayer('house');
   map.addTilesetImage('maptile', 'tileset');
+  game.world.setBounds(0, 0, 480, 360);
 
   // sprite
-  this.player = this.game.add.sprite(game.world.centerX, game.world.centerY                                         , 'sprite');
-  this.player.scale.setTo(2, 2);
-  this.player.anchor.set(0.5, 0.5);
-  this.player.frame = 0;
-  this.player.animations.add('left', [0], 2, true);
-  this.player.animations.add('left', [1, 2, 3], 2, false);
-  this.physics.arcade.enable(this.player);
+  self.player = this.game.add.sprite(240, 180, 'sprite');
+  game.add.existing(self.player);
+  game.physics.enable(self.player, Phaser.Physics.ARCADE);
+  game.camera.x = 480;
+  game.camera.y = 360;
+  self.player.scale.setTo(2, 2);
+  self.player.anchor.set(0.5, 0.5);
+  self.player.frame = 0;
+  self.player.animations.add('idle', [0], 2, true);
+  self.player.animations.add('left', [1, 2, 3], 2, false);
   // capture
   game.input.activePointer.capture = true;
 
@@ -62,28 +44,52 @@ function create() {
 }
 
 function update() {
-  let k = this.input.keyboard;
-  this.physics.arcade.collide(this.player, this.house);
-  if(k.isDown(37)){
-    this.player.x -= 2;
-    this.player.animations.play('left');
-      this.player.scale.setTo(2, 2);
-  }
-  else if(k.isDown(39)){
-    this.player.x += 2;
-    this.player.scale.setTo(-2, 2);
-    this.player.animations.play('left');
-  }
-  else if(k.isDown(38)){
-    this.player.y -= 2;
-  }
-  else if(k.isDown(40)){
-    this.player.y += 2;
-  }
+//  let k = this.input.keyboard;
+//  if(k.isDown(37)){
+//    self.player.x -= 2;
+//    self.player.animations.play('left');
+//      self.player.scale.setTo(2, 2);
+//  }
+//  else if(k.isDown(39)){
+//    self.player.x += 2;
+//    self.player.scale.setTo(-2, 2);
+//    self.player.animations.play('left');
+//  }
+//  else if(k.isDown(38)){
+//    self.player.y -= 2;
+//  }
+//  else if(k.isDown(40)){
+//    self.player.y += 2;
+//  }
   // mouse/touchscreen input
+  self.player.animations.play('idle');
   if(game.input.activePointer.isDown){
-    this.player.xDest = game.input.x;
-    this.player.yDest = game.input.y;
+    self.player.xDest = game.input.x;
+    self.player.yDest = game.input.y;
   }
-  //this.movePlayer(game.input.x, game.input.y);
+  self.movePlayer(game.input.x, game.input.y);
+}
+
+function movePlayer(){
+  if (Math.floor(self.x / 10) == Math.floor(self.xDest / 10)) {
+    self.body.velocity.x = 0;
+  } else if (Math.floor(self.x) < Math.floor(self.xDest)) {
+    self.body.velocity.x = 80;
+    self.player.scale.setTo(-2, 2);
+  } else if (Math.floor(self.x) > Math.floor(self.xDest)) {
+    self.body.velocity.x = -80;
+    self.player.scale.setTo(2, 2);
+  }
+  if (Math.floor(self.y / 10) === Math.floor(self.yDest / 10)) {
+    self.body.velocity.y = 0;
+  } else if (Math.floor(self.y) < Math.floor(self.yDest)) {
+    self.body.velocity.y = 80;
+  } else if (Math.floor(self.y) > Math.floor(self.yDest)) {
+    self.body.velocity.y = -80;
+  }
+}
+
+player.update = function(){
+  game.camera.x = self.player.x - 150;
+  game.camera.y = self.player.y - 100;
 }
