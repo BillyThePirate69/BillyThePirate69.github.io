816bd4262cdc6d8febfa4f8cab20ffa1646b0f70
BillyThePirate69 <36053848+BillyThePirate69@users.noreply.github.com>
2018-06-11 15:19:56 -0600

    Add files via upload

--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -16,7 +16,7 @@ function preload() {
   game.load.spritesheet('enemy', 'sprite/goblin/0.png', 20, 51);
   game.load.spritesheet('sprite', 'sprite/walkleft/0.png', 34, 34);
   game.load.image('tileset', 'map/maptile.png');
-  game.load.tilemap('map', 'map/house.json', null, Phaser.Tilemap.TILED_JSON);
+  game.load.tilemap('map', 'map/collision.json', null, Phaser.Tilemap.TILED_JSON);
 
 }
 
@@ -26,43 +26,34 @@ function create() {
   this.layer = map.createLayer('grass');
   this.house = map.createLayer('house');
   map.addTilesetImage('maptile', 'tileset');
-  game.world.setBounds(0, 0, 480, 360);
+  //this.world.setBounds(0, 0, );
 
   //sprite goblin
-  self.enemy = this.game.add.sprite(120, 120, 'enemy');
-  game.add.existing(self.enemy);
-  game.physics.enable(self.enemy, Phaser.Physics.ARCADE);
-  self.enemy.anchor.set(0.5, 0.5);
-  self.enemy.scale.setTo(0.8, 0.8);
+  this.enemy = this.game.add.sprite(120, 120, 'enemy');
+  game.add.existing(this.enemy);
+  game.physics.enable(this.enemy, Phaser.Physics.ARCADE);
+  this.enemy.anchor.set(0.5, 0.5);
+  this.enemy.scale.setTo(0.8, 0.8);
 
-  // sprite player
-  self.player = this.game.add.sprite(240, 180, 'sprite');
+  //player class
+  self.player = new Player(150, 100);
+  game.camera.x = 300;
+  game.camera.y = 200;
   game.add.existing(self.player);
   game.physics.enable(self.player, Phaser.Physics.ARCADE);
-  game.camera.x = 120;
-  game.camera.y = 100;
-  self.player.scale.setTo(2, 2);
-  self.player.anchor.set(0.5, 0.5);
-  self.player.animations.add('idle', [0], 2, true);
-  self.player.animations.add('left', [1, 2, 3], 2, false);
 
-  // player class
+  //this.camera.follow(this.player, Phaser.Camera.FOLLOW_TOPDOWN, 1, 1);
 
-  // capture
   game.input.activePointer.capture = true;
-
-
 }
 
 function update() {
   if(game.input.activePointer.isDown){
-    self.player.xDest = game.input.x;
-    self.player.yDest = game.input.y;
+    player.setDest(game.input.x - game.world.worldPosition.x, game.input.y - game.world.worldPosition.y);
   }
-  self.movePlayer(game.input.x, game.input.y);
 }
 
-function movePlayer(){
+function movePlayer(self){
   if (Math.floor(self.player.x / 10) == Math.floor(self.player.xDest / 10)) {
     self.player.body.velocity.x = 0;
   } else if (Math.floor(self.player.x) < Math.floor(self.player.xDest)) {
@@ -89,8 +80,24 @@ function stopPlayer(){
   self.player.body.velocity.x = self.player.body.velocity.y = 0;
 }
 
+function Player(x, y){
+  let player = game.add.sprite(x, y, 'sprite');
+  player.speed = 80;
+  player.xDest = x;
+  player.yDest = y;
+  player.scale.setTo(2, 2);
+  player.anchor.set(0.5, 0.5);
+  player.animations.add('idle', [0], 2, true);
+  player.animations.add('left', [1, 2, 3], 2, false);
+}
+
+player.setDest = function(x, y){
+  player.xDest = x;
+  player.yDest = y;
+}
 
 player.update = function(){
+  movePlayer(self);
   game.camera.x = self.player.x - 150;
   game.camera.y = self.player.y - 100;
 }
